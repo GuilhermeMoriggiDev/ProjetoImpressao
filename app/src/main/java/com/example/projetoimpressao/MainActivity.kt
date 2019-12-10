6aebfe3eb2f63774d574e60d7dee7a3aacb1977c
henriquebalmeida <54820168+henriquebalmeida@users.noreply.github.com>
2019-12-10 17:17:56 -0300

    WIFI FUNFANDO

--- app/src/main/java/com/example/projetoimpressao/MainActivity.kt
+++ app/src/main/java/com/example/projetoimpressao/MainActivity.kt
@@ -11,14 +11,25 @@ import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Parcelable
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.felhr.usbserial.UsbSerialDevice
 import com.felhr.usbserial.UsbSerialInterface
 import com.github.anastaciocintra.escpos.EscPos
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintStream
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import java.net.Socket
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,32 +37,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val bluetooth = getBondedDevices()
-//        for (device: BluetoothDevice in bluetooth!!.lista) {
-//            if (device.name == "TM-Enterplug") {
-//                val bluetoothDevice = BluetoothDeviceSocketConnexion(device)
-//                bluetoothDevice.connect()
-//                val printerOutputStream = bluetoothDevice.bluetoothSocket!!.outputStream
-//                val escpos = EscPos(printerOutputStream)
-//                escpos.writeLF(
-//                    "aaa!"
-//                )
-//                escpos.feed(8)
-//                escpos.cut(EscPos.CutMode.FULL)
-//            }
-//
-//        }
+
+        bluetooth.setOnClickListener {
+            val bluetooth = getBondedDevices()
+            for (device: BluetoothDevice in bluetooth!!.lista) {
+                if (device.name == "TM-Enterplug") {
+                    val bluetoothDevice = BluetoothDeviceSocketConnexion(device)
+                    bluetoothDevice.connect()
+                    val printerOutputStream = bluetoothDevice.bluetoothSocket!!.outputStream
+                    val escpos = EscPos(printerOutputStream)
+                    escpos.writeLF(
+                        "aaa!"
+                    )
+                    escpos.feed(8)
+                    escpos.cut(EscPos.CutMode.FULL)
+                }
+
+            }
+        }
+
         val manager = this.getSystemService(Context.USB_SERVICE) as UsbManager
         val usbPermissionName = "com.android.example.USB_PERMISSION";
         var connection: UsbDeviceConnection? = null
-        registerReceiver(object: BroadcastReceiver() {
+        registerReceiver(object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 if (usbPermissionName.equals(intent!!.action)) {
                     val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
-                    if(intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                        synchronized (this) {
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        synchronized(this) {
                             connection = manager.openDevice(device)
-                            if(connection != null) {
+                            if (connection != null) {
                                 val usbSerialDevice =
                                     UsbSerialDevice.createUsbSerialDevice(
                                         device,
@@ -63,14 +78,22 @@ class MainActivity : AppCompatActivity() {
                                 usbSerialDevice.setParity(UsbSerialInterface.PARITY_ODD)
                                 usbSerialDevice.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
                                 val escpos = EscPos(usbSerialDevice.outputStream)
-                                Toast.makeText(this@MainActivity, "SEU PAI TEM GONORREIA", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "SEU PAI TEM GONORREIA",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                                 escpos.writeLF(
                                     "Sds dela mano"
                                 )
                                 escpos.feed(3)
                                 escpos.cut(EscPos.CutMode.PART)
-                            }else{
-                                Toast.makeText(this@MainActivity, "Não foi possível conectar", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "Não foi possível conectar",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
                     }
@@ -81,8 +104,32 @@ class MainActivity : AppCompatActivity() {
         usb.setOnClickListener {
             Toast.makeText(this@MainActivity, "SEU PAI TEM GONORREIA", Toast.LENGTH_SHORT).show()
             manager.deviceList.forEach {
-                val permissionPendingIntent = PendingIntent.getBroadcast(this, 0, Intent(usbPermissionName), 0)
+                val permissionPendingIntent =
+                    PendingIntent.getBroadcast(this, 0, Intent(usbPermissionName), 0)
                 manager.requestPermission(it.value, permissionPendingIntent);
+            }
+        }
+
+        wifi.setOnClickListener {
+            val coroutineScope = CoroutineScope(Job() + Dispatchers.IO)
+            coroutineScope.launch {
+                val port = 80
+                var listener: ServerSocket?
+                try {
+                    var socketAdress = InetSocketAddress("192.168.0.128", 9100)
+                    val socket2 = Socket()
+                    socket2.connect(socketAdress, 5000)
+                    val escpos = EscPos(socket2.getOutputStream())
+                    escpos.writeLF(
+                        "Sds dela mano"
+                    )
+                    escpos.feed(3)
+                    escpos.cut(EscPos.CutMode.PART)
+                    socket2.close()
+
+                } catch (e: IOException) {
+                    Log.d("teste", e.toString())
+                }
             }
         }
     }
